da2ce40137ed36176f940ec2a56a465a8c7e0073
Harshavardhanmr <your-actual-github-email@example.com>
2026-09-20 18:46:59 +0530

    update CORS with actual Vercel URL

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://fullstack-login-app-31m1.onrender.com'],
+  origin: ['http://localhost:5173', 'https://fullstack-login-app-seven.vercel.app'],
   credentials: true
 }));
 app.use('/api/auth', authRoutes);
